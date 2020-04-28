b590a113d14fe673d28aa000d6f3a6b2ea23ec2a
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-04-28 09:23:44 -0500

    agregando los paraemtros  en el response

--- fn-beacons.cs
+++ fn-beacons.cs
@@ -21,52 +21,40 @@ namespace Beacons.Function
     
     public static class fn_beacons
     {
-
+// -----------------INSTANCIANDO-----------------------------------------//
         private static CosmosClient cosmosClient;
         private static Database database;
         private static Container container;
         private static String endpointUri = "https://localhost:8081";
         private static String primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-        // private static string databaseId = "taskDatabase";
-        // private static string collectionId = "TaskCollection";
-        // private static string containerId = "Items";
-
         private static string databaseId = "BeaconsDatabase";
-        //private static string collectionId = "collectionBeacons";
+        
         private static string containerId = "BeaconsContainer";
-
+//-------------------------------------FUNCION----------------------------------------//
         [FunctionName("fn_beacons")]
         public static async Task<IActionResult> Run(
-            //[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("==================================================================================================================================");
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            // GET
-            //string mac = req.Query["mac"];
-
-            // POST
+           
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            /*
-            mac = mac ?? data?.name;
-             
-            return mac != null
-                ? (ActionResult)new OkObjectResult($"Hello, {mac}")
-                : new BadRequestObjectResult("Please pass a mac on the query string or in the request body"); */
-
+      
+//--------------------------INSTANCIANDO COSMOS CLIENT--------------------------------//
             cosmosClient = new CosmosClient(endpointUri, primaryKey, new CosmosClientOptions() { ApplicationName = "BeaconsACN" });
 
-            //var sqlQueryText = "SELECT * FROM c WHERE c.LastName = 'Andersen'";
-
+//-----------------------------CREANDO LA BASE DE DATOS---------------------------------//
             database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
             log.LogInformation("Created Database: {0}\n", database.Id);
+//-----------------------------CREANDO EL CONTENEDOR---------------------------//          
             container = await database.CreateContainerIfNotExistsAsync(containerId, "/mac", 400);
             log.LogInformation("Created Container: {0}\n", container.Id);
-
+//----------------------------INSTANCIANDO BEACONS Y CREANDO EL ITEM-----------// 
             Beacons CreateItemBeacons = new Beacons
             {
                 address = data?.address,
@@ -77,7 +65,7 @@ namespace Beacons.Function
 
             try
             {
-                // Read the item to see if it exists.  
+                //  Lea el item para ver si existe. 
                 ItemResponse<Beacons> CreateItemBeaconsResponse = await container.ReadItemAsync<Beacons>(CreateItemBeacons.mac, new PartitionKey(CreateItemBeacons.mac));
                 log.LogInformation(">>>");
                 log.LogInformation("Item in database with id: {0} already exists\n", CreateItemBeaconsResponse.Resource.mac);
@@ -85,59 +73,51 @@ namespace Beacons.Function
             }
             catch(CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
             {
-                // Create an item in the container 
+                // Crea un item en el contenedor 
                 ItemResponse<Beacons> CreateItemBeaconsResponse = await container.UpsertItemAsync<Beacons>(CreateItemBeacons, new PartitionKey(CreateItemBeacons.mac));
                 log.LogInformation(">>>");
                 log.LogInformation("Created item in database with id: {0} Operation consumed {1} RUs.\n", CreateItemBeaconsResponse.Resource.mac, CreateItemBeaconsResponse.RequestCharge);
                 
             }
-
+//----------------------------CONSULTANDO LA BASE DE DATOS-------------------------------//
             var sqlQueryText = "SELECT * FROM c";
 
             log.LogInformation(">>>");
             log.LogInformation("Running query: {0}\n", sqlQueryText);
-            //Console.WriteLine("Running query: {0}\n", sqlQueryText);
+            
 
             QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
             FeedIterator<Beacons> queryResultSetIterator = container.GetItemQueryIterator<Beacons>(queryDefinition);
 
-            List<Beacons> beaconsindb = new List<Beacons>();
+            List<Beacons> beaconQ = new List<Beacons>();
 
             while (queryResultSetIterator.HasMoreResults)
             {
                 FeedResponse<Beacons> currentResultSet = await queryResultSetIterator.ReadNextAsync();
                 foreach (Beacons beacon_x in currentResultSet)
                 {
-                    beaconsindb.Add(beacon_x);
+                    beaconQ.Add(beacon_x);
                     log.LogInformation(">>>");
                     log.LogInformation("\tRead {0}\n", beacon_x);
                     
                 }
             }
 
-           /*
-            foreach (var item in collection)
-            {
-                
-            }
-             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://www.google.com");
-            var result = await client.GetAsync("");
-            string resultContent = await result.Content.ReadAsStringAsync(); */
             
-            
-            string response = "{\"address\": \"F8:F0:05:C0:74:09\", \"class\": 7936, \"id\": \""+ data?.mac +"\", \"name\": null, \"status\":200, \"message\":\"OK\"}";
+            string response = "{\"address\": \""+ data?.address +"\", \"class\": \""+ data?.klass +"\", \"mac\": \""+ data?.mac +"\", \"name\": \""+ data?.name +"\", \"status\":200, \"message\":\"OK\"}";
 
             string responseMessage;
             
             if (data?.mac == null){
                 responseMessage = "{ \"status\":400, \"message\":\"Error HTTP 400 (Bad Request)\"}";
+                log.LogError(responseMessage);
             }
             else{
                 responseMessage = response;
+                log.LogInformation(response);
             }
                 
-            return new OkObjectResult(beaconsindb);
+            return new OkObjectResult(beaconQ);
         }
     }
 
